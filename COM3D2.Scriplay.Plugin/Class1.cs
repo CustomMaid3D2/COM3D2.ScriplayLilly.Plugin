3a6507ca12f4455c5a0eda1ec6038b59263f21d2
lilly <lilly1987@naver.com>
2019-03-09 01:48:24 +0900

    초기 커밋.

---
+++ COM3D2.Scriplay.Plugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM3D2.Scriplay.Plugin
+{
+    public class Class1
+    {
+    }
+}
